f87edda68be82ac32e639bce7c56c371146c3ffc
Giovany7x <u202311601@upc.edu.pe>
2025-10-08 14:01:00 -0500

    feat(login):update colors

--- app/src/main/java/pe/edu/upc/flota365/ui/theme/Color.kt
+++ app/src/main/java/pe/edu/upc/flota365/ui/theme/Color.kt
@@ -2,10 +2,29 @@ package pe.edu.upc.flota365.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryTeal = Color(0xFF00B6C9)
+val PrimaryTealDark = Color(0xFF008A99)
+val PrimaryTealContainer = Color(0xFFB2EBF2)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SecondaryAmber = Color(0xFFFFC107)
+val SecondaryAmberContainer = Color(0xFFFFECB3)
+
+val AccentTeal = Color(0xFF007F8C)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceVariantLight = Color(0xFFE0E5EC)
+val BackgroundLight = Color(0xFFF4F6F8)
+val TextPrimaryDark = Color(0xFF1C1F25)
+val TextSecondaryDark = Color(0xFF434A54)
+
+val PrimaryTealDarkMode = Color(0xFF00A5B8)
+val PrimaryTealContainerDark = Color(0xFF004F58)
+val SecondaryAmberDark = Color(0xFFE0A806)
+val SecondaryAmberContainerDark = Color(0xFF4D3800)
+val SurfaceDark = Color(0xFF181C20)
+val SurfaceVariantDark = Color(0xFF2A3036)
+val BackgroundDark = Color(0xFF101417)
+val TextPrimaryLight = Color(0xFFE0E3E7)
+val TextSecondaryLight = Color(0xFFB2BAC4)
+
+val OutlineLight = Color(0xFF96A2AE)
+val OutlineDark = Color(0xFF7F8893)
